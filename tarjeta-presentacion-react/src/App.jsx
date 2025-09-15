8f12da91deb45a4bb55e16a672819517afdbed42
Aldo <aldourielbec@gmail.com>
2025-09-15 12:37:52 -0600

    Proyecto Tarjeta de presentación

---
+++ tarjeta-presentacion-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Becerril Martinez Aldo uriel</h1>
+      <h2>Estudiante de Ciencias de la Informática</h2>
+      <p>💻</p>
+    </div>
+  );
+}
+
+export default App;
